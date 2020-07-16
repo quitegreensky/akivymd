d5d11ec73ac7629fdefb283238647b657dce2ad4
quitegreensky <quitegreensky@gmail.com>
2020-07-16 21:39:10 +0430

    Fix statusbar in demo app

--- demo/screens/statusbarcolor.py
+++ demo/screens/statusbarcolor.py
@@ -6,7 +6,7 @@ Builder.load_string(
     """
 <StatusbarColor>:
     name: 'StatusbarColor'
-    on_leave: root.change_color( app.theme_cls.primary_color )
+    on_leave: root.change_color( app.theme_cls.primary_color , 'Light' )
     BoxLayout:
         orientation: 'vertical'
         MDToolbar:
