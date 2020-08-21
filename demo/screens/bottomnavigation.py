99d7c2b8b0ab297bb3a429b68ce03bbb5ff201fe
quitegreensky <quitegreensky@gmail.com>
2020-08-21 14:10:39 +0430

    Add new BottomNavigation2

--- demo/screens/bottomnavigation.py
+++ demo/screens/bottomnavigation.py
@@ -2,6 +2,7 @@ from akivymd.uix.bottomnavigation import AKBottomNavigation
 from kivy.lang import Builder
 from kivy.uix.screenmanager import Screen
 from kivymd.toast import toast
+from akivymd.uix.bottomnavigation2 import AKBottomNavigation2
 
 Builder.load_string(
 '''
@@ -10,11 +11,47 @@ Builder.load_string(
     name: 'BottomNavigation'
     BoxLayout:
         orientation: 'vertical'
+        spacing: dp(40)
         MDToolbar:
             title: root.name
             left_action_items:[['arrow-left' , lambda x:app.show_screen('Home','back') ]]
         BoxLayout:
             orientation: 'vertical'
+
+        AKBottomNavigation2:
+            bg_color: app.theme_cls.bg_dark
+
+            Button_Item:
+                text: 'Alert'
+                icon: 'bell-outline'
+                icon_color: 0.3,0.2,0.3 ,1
+                text_color: 0.3,0.2,0.3 ,1
+                button_bg_color: 0.7,0.5,0.7,1
+                badge_text: '+12'
+
+            Button_Item:
+                text: 'Bank'
+                badge_text: ''
+                icon: 'bank-outline'
+                icon_color: 0.2,0.2,0.6 ,1
+                text_color: 0.2,0.2,0.6 ,1
+                button_bg_color: 0.6,0.6,1,1
+
+            Button_Item:
+                text: 'Download'
+                icon: 'arrow-down-bold-outline'
+                icon_color: 0.8,0,0 ,1
+                text_color: 0.8,0,0 ,1
+                button_bg_color: 1,0.6,0.6 ,1    
+                badge_disabled: True 
+
+            Button_Item:
+                text: 'Account'
+                icon: 'account-circle-outline'
+                icon_color: 0,0.4,0 ,1
+                text_color: 0,0.4,0 ,1
+                button_bg_color: 0.4,1,0.4 ,1 
+                badge_text: '+2'  
 
         AKBottomNavigation:
             items: root.bottomnavigation_items
