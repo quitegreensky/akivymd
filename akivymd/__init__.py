530abce8ceb4c71080c020b417cc7573b2542ab6
quitegreensky <quitegreensky@gmail.com>
2020-08-23 15:15:01 +0430

    Version 1.1

--- akivymd/__init__.py
+++ akivymd/__init__.py
@@ -1,6 +1,6 @@
 from kivy.logger import Logger
 
-__version__ = 1.0
+__version__ = 1.1
 __description__ = 'A set of fancy widgets for KivyMD'
 __author__= "Sina Namadian"
 __email__="quitegreensky@gmail.com"
