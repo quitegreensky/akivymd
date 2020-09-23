f896172b78ebaccdd4fad748d469ec4d8cf1f00e
quitegreensky <quitegreensky@gmail.com>
2020-09-23 11:01:23 +0330

    Fix setup.py

--- setup.py
+++ setup.py
@@ -1,25 +1,44 @@
 
 from setuptools import setup, find_packages
-import akivymd
-requirements_path='requirements.txt'
+import os 
 
+requirements_path='requirements.txt'
 with open("README.md", "r") as fh:
     long_description = fh.read()
+
+def get_package_info():
+
+    init_path = os.path.join(os.path.dirname(__file__) , 'akivymd', '__init__.py')
+    info_dic = {
+        '__version__': '' , 
+        '__description__': '' ,
+        '__author__': '', 
+        '__email__': ''
+        }
+
+    with open(init_path, 'rt') as info:
+        for line in info :
+            for info in info_dic.keys():
+                if line.startswith(info):
+                    info_dic[info]= line.split('=')[1]
+                    continue
+    return info_dic
 
 def parse_requirements(filename):
     lineiter = (line.strip() for line in open(filename))
     return [line for line in lineiter if line and not line.startswith("#")]
 
 install_reqs = parse_requirements(requirements_path)
+package_info = get_package_info()
 setup(
     name='akivymd',
-    version=str(akivymd.__version__),
-    description=akivymd.__description__,
+    version= package_info['__version__'],
+    description= package_info['__description__'],
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/quitegreensky/akivymd",
-    author=akivymd.__author__,
-    author_email=akivymd.__email__,
+    author= package_info['__author__'],
+    author_email= package_info['__email__'],
     license='MIT',
     install_requires=parse_requirements(requirements_path) ,
     packages=find_packages(),
