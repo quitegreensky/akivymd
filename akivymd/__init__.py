c32feaace0b672d6bc52f433efa4b8edee9d189f
quitegreensky <quitegreensky@gmail.com>
2020-10-08 15:31:26 +0330

    Version 1.2

--- akivymd/__init__.py
+++ akivymd/__init__.py
@@ -2,7 +2,7 @@ from akivymd.tools.pyinstaller_hooks import hooks_path
 import akivymd.factory_registers
 from kivy.logger import Logger
 
-__version__ = 1.1
+__version__ = 1.2
 __description__ = 'A set of fancy widgets for KivyMD'
 __author__ = "Sina Namadian"
 __email__ = "quitegreensky@gmail.com"
